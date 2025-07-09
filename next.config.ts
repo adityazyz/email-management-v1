0b2e5c0c23bb9926f311f9b5aaa31532963d7e6f
adityazyzz <tuskersadi@gmail.com>
2025-07-09 14:52:28 +0530

    fully working model ( but slow when  heavy attachment ( > 1 mb)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      // A list of extra safe origin domains from which Server Actions can be invoked
+      allowedOrigins: ['my-proxy.com', '*.my-proxy.com'],
+      
+      // Maximum size of the request body sent to a Server Action
+      // Default is 1MB, can be number of bytes or string format like '2mb', '500kb'
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
